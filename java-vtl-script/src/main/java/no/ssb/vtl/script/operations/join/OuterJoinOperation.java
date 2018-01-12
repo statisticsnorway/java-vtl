024f4261cbef54d62ecb61a73d199e08f2de418c
Hadrien Kohl <hadrien.kohl@gmail.com>
2018-01-12 23:01:21 +0100

    Fix OuterJoinOperation indices map

--- java-vtl-script/src/main/java/no/ssb/vtl/script/operations/join/OuterJoinOperation.java
+++ java-vtl-script/src/main/java/no/ssb/vtl/script/operations/join/OuterJoinOperation.java
@@ -9,9 +9,9 @@ package no.ssb.vtl.script.operations.join;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -39,9 +39,8 @@ package no.ssb.vtl.script.operations.join;
  * #L%
  */
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
+import com.google.common.collect.Table;
 import no.ssb.vtl.model.Component;
 import no.ssb.vtl.model.DataPoint;
 import no.ssb.vtl.model.DataStructure;
@@ -68,33 +67,28 @@ public class OuterJoinOperation extends AbstractJoinOperation {
             final Dataset leftDataset, final Dataset rightDataset
     ) {
 
-        DataStructure structure = getDataStructure();
-        ImmutableList<Component> leftList = ImmutableList.copyOf(structure.values());
-
+        DataStructure leftStructure = getDataStructure();
         DataStructure rightStructure = rightDataset.getDataStructure();
-        ImmutableList<Component> rightList = ImmutableList.copyOf(rightStructure.values());
 
         // Save the indexes of the right data point that need to be moved to the left.
         final ImmutableMap<Integer, Integer> indexMap;
 
-        Map<Integer, Integer> indexMapBuilder = Maps.newHashMap();
-        for (int i = rightList.size() - 1; i >= 0; --i) {
-            Component rightComponent = rightList.get(i);
-            for (int j = leftList.size() - 1; j >= 0 ; --j) {
-                Component leftComponent = leftList.get(j);
-                if (rightComponent.equals(leftComponent)) {
-                    indexMapBuilder.putIfAbsent(i, j);
-                }
-            }
+        ImmutableMap.Builder<Integer, Integer> indexMapBuilder = ImmutableMap.builder();
+        Table<Component, Dataset, Component> mapping = getComponentMapping();
+        Set<Map.Entry<Component, Component>> leftToRightComponentMapping = mapping.column(rightDataset).entrySet();
+        for (Map.Entry<Component, Component> entry : leftToRightComponentMapping) {
+            Component rightComponent = entry.getValue();
+            Component leftComponent = entry.getKey();
+            indexMapBuilder.put(rightStructure.indexOf(rightComponent), leftStructure.indexOf(leftComponent));
         }
-        indexMap = ImmutableMap.copyOf(indexMapBuilder);
+        indexMap = indexMapBuilder.build();
 
         return (left, right) -> {
 
             /*
              * We overwrite the ids if right != null for simplicity.
              */
-            DataPoint result = left != null ? DataPoint.create(left) : DataPoint.create(structure.size());
+            DataPoint result = left != null ? DataPoint.create(left) : DataPoint.create(leftStructure.size());
 
             if (right != null) {
                 for (Map.Entry<Integer, Integer> entry : indexMap.entrySet())
